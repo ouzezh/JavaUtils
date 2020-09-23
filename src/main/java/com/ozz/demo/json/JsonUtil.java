9f7bfff337b7cda2a03d6c5c777ae033165f7940
ouzezh <ouzezh@163.com>
2020-09-23 13:11:11 +0800

    json

--- src/main/java/com/ozz/demo/json/JsonUtil.java
+++ src/main/java/com/ozz/demo/json/JsonUtil.java
@@ -12,28 +12,34 @@ public class JsonUtil {
     // 链接
     String rowsJson =
         "[{\"id\":\"id1\",\"name\":\"name1\",\"date\":\"2018-08-08 08:08\",\"localDateTime\":\"2019-09-09 09:09:09\"}]";
-    List<Item> rows = fromJson(rowsJson, new TypeReference<List<Item>>() {
+    List<Item> rows = parseObject(rowsJson, new TypeReference<List<Item>>() {
     });
-    System.out.println(JsonUtil.toJson(rows));
+    // 链接2
+    rows = parseArray(rowsJson, Item.class);
+    System.out.println(JsonUtil.toJSONString(rows));
     // 复杂对象
-    Page<Item> page = fromJson("{\"rows\":" + rowsJson + "}", new TypeReference<Page<Item>>() {
+    Page<Item> page = parseObject("{\"rows\":" + rowsJson + "}", new TypeReference<Page<Item>>() {
     });
-    System.out.println(JsonUtil.toJson(page));
+    System.out.println(JsonUtil.toJSONString(page));
   }
 
-  public static String toJson(Object object) {
+  public static String toJSONString(Object object) {
 //    JSON.toJSONString(object, SerializerFeature.WriteMapNullValue);//是否输出值为null的字段,默认为false
     return JSON.toJSONString(object);
   }
 
-  public static <T> T fromJson(String text, Class<T> clazz) {
+  public static <T> T parseObject(String text, Class<T> clazz) {
     return JSON.parseObject(text, clazz);
   }
 
   /**
    * 解析复杂格式
    */
-  public static <T> T fromJson(String text, TypeReference<T> typeReference) {
+  public static <T> T parseObject(String text, TypeReference<T> typeReference) {
     return JSON.parseObject(text, typeReference);
+  }
+
+  public static <T> List<T> parseArray(String text, Class<T> clazz) {
+    return JSON.parseArray(text, clazz);
   }
 }
