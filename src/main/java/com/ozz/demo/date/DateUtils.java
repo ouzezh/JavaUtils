81a3f24dea35fb14a0fde404319445ee77930752
ouzezhou <ouzezhou@xdf.cn>
2024-09-03 14:46:59 +0800

    u

--- src/main/java/com/ozz/demo/date/DateUtils.java
+++ src/main/java/com/ozz/demo/date/DateUtils.java
@@ -2,6 +2,7 @@ package com.ozz.demo.date;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.date.LocalDateTimeUtil;
+import cn.hutool.core.lang.Assert;
 import lombok.SneakyThrows;
 
 import java.time.Duration;
@@ -12,54 +13,53 @@ import java.util.Date;
 
 public class DateUtils {
 
-  public static String format(LocalDateTime ldt) {
-      return LocalDateTimeUtil.formatNormal(ldt);
-  }
+    public static String format(LocalDateTime ldt) {
+        return LocalDateTimeUtil.formatNormal(ldt);
+    }
 
-  public static LocalDateTime toLocalDateTime(Date date) {
-    return DateUtil.toLocalDateTime(date);
-  }
+    public static LocalDateTime toLocalDateTime(Date date) {
+        return DateUtil.toLocalDateTime(date);
+    }
 
-  public static Date toDate(LocalDateTime localDateTime) {
-    return new Date(LocalDateTimeUtil.toEpochMilli(localDateTime));
-  }
+    public static Date toDate(LocalDateTime localDateTime) {
+        return new Date(LocalDateTimeUtil.toEpochMilli(localDateTime));
+    }
 
-  public static ZoneId getZoneId() {
-    return ZoneId.systemDefault();
-  }
+    public static ZoneId getZoneId() {
+        return ZoneId.systemDefault();
+    }
 
-  @SneakyThrows
-  public static Date parseDate(String dateString) {
-    return DateUtil.parseDate(dateString);
-  }
+    @SneakyThrows
+    public static Date parseDate(String dateString) {
+        return DateUtil.parseDate(dateString);
+    }
 
-  public static long between(LocalDate d1, LocalDate d2) {
-      return d2.toEpochDay() - d1.toEpochDay();
-  }
-  public static Duration between(LocalDateTime d1, LocalDateTime d2) {
-      return LocalDateTimeUtil.between(d1, d2);
-  }
+    public static long between(LocalDate d1, LocalDate d2) {
+        return d2.toEpochDay() - d1.toEpochDay();
+    }
 
-  public static String formatBetween(long millis) {
+    public static Duration between(LocalDateTime d1, LocalDateTime d2) {
+        return LocalDateTimeUtil.between(d1, d2);
+    }
+
+    public static String formatBetween(long millis) {
 //      return Duration.ofMillis(millis).toString();
-      return DateUtil.formatBetween(millis);
-  }
-
-  public static String getExpectedTime(long startTime, int currentNum, int totalNum) {
-    int passedNum = currentNum - 1;
-    long costTime = System.currentTimeMillis() - startTime;
-    StringBuilder mess =
-        new StringBuilder(String.valueOf(currentNum)).append(" of ").append(totalNum)
-            .append(", cost ").append(formatBetween(costTime));
-    if (passedNum <= 0) {
-      return mess.toString();
+        return DateUtil.formatBetween(millis);
     }
 
-    if (totalNum >= currentNum) {
-      mess.append(", left ")
-          .append(formatBetween((totalNum - passedNum) * (costTime / passedNum)));
+    public static String getExpectedTime(long startTimeMillis, int passedCou, int totalCou) {
+        Assert.isTrue(passedCou > 0);
+        long costTime = System.currentTimeMillis() - startTimeMillis;
+        StringBuilder mess =
+                new StringBuilder(String.valueOf(passedCou)).append(" of ").append(totalCou)
+                        .append(", cost ").append(formatBetween(costTime));
+
+        if (totalCou >= passedCou) {
+            int leftCou = totalCou - passedCou;
+            mess.append(", left ")
+                    .append(formatBetween((leftCou) * (costTime / passedCou)));
+        }
+        return mess.toString();
     }
-    return mess.toString();
-  }
 
 }
