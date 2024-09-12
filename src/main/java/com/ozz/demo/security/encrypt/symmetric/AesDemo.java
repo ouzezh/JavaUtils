9ba346a4c693b3812a0c6db0762addfeae4774a0
ouzezhou <ouzezhou@xdf.cn>
2024-09-12 11:44:13 +0800

    u

--- src/main/java/com/ozz/demo/security/encrypt/symmetric/AesDemo.java
+++ src/main/java/com/ozz/demo/security/encrypt/symmetric/AesDemo.java
@@ -2,7 +2,6 @@ package com.ozz.demo.security.encrypt.symmetric;
 
 import cn.hutool.core.codec.Base64;
 import cn.hutool.core.lang.Assert;
-import cn.hutool.core.util.CharsetUtil;
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.crypto.Mode;
 import cn.hutool.crypto.Padding;
@@ -31,16 +30,18 @@ public class AesDemo {
         log.info("iv: " + iv);
 
         // 加密解密
-        String encryptHex = encryptBase64(data, key);
-        String decryptStr = decryptStr(encryptHex, key);
+        AES aesECB = getAesECB(key);
+        String encryptHex = aesECB.encryptBase64(data);
+        String decryptStr = aesECB.decryptStr(encryptHex);
         // 打印
         log.info("密文: " + encryptHex);
         log.info("明文: " + decryptStr);
         Assert.isTrue(data.equals(decryptStr));
 
         // 带初始化向量(iv)加密解密：前端加密密文初始化向量通常是加密数据的前16个字节
-        String encryptHexCBC = encryptBase64CBC(data, key, iv);
-        String decryptStrCBC = decryptStrCBC(encryptHexCBC, key, iv);
+        AES aesCBC = getAesCBC(key, iv);
+        String encryptHexCBC = aesCBC.encryptBase64(data);
+        String decryptStrCBC = aesCBC.decryptStr(encryptHexCBC);
         // 打印
         log.info("带iv密文: " + encryptHexCBC);
         log.info("带iv明文: " + decryptStrCBC);
@@ -48,28 +49,16 @@ public class AesDemo {
 
     }
 
-    private static String decryptStrCBC(String data, String key, String iv) {
-        AES aes = new AES(Mode.CBC.name(), "PKCS7Padding", Base64.decode(key), Base64.decode(iv));
-        return aes.decryptStr(data);
-    }
-
-    private static String encryptBase64CBC(String data, String key, String iv) {
-        AES aes = new AES(Mode.CBC.name(), "PKCS7Padding", Base64.decode(key), Base64.decode(iv));
-        return aes.encryptBase64(data);
-    }
-
     public static byte[] randomBytes(int length) {
         return RandomUtil.randomBytes(length);
     }
 
-    private static String decryptStr(String data, String key) {
-        AES aes = new AES(Mode.ECB, Padding.PKCS5Padding, Base64.decode(key));
-        return aes.decryptStr(data, CharsetUtil.CHARSET_UTF_8);
+    private static AES getAesCBC(String key, String iv) {
+        return new AES(Mode.CBC.name(), "PKCS7Padding", Base64.decode(key), Base64.decode(iv));
     }
 
-    private static String encryptBase64(String data, String key) {
-        AES aes = new AES(Mode.ECB, Padding.PKCS5Padding, Base64.decode(key));
-        return aes.encryptBase64(data);
+    private static AES getAesECB(String key) {
+        return new AES(Mode.ECB, Padding.PKCS5Padding, Base64.decode(key));
     }
 
 }
